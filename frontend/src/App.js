d0bcdb13475699751ff695fc34406e0280e5a926
ariellopesdev <lubyel.lopes@gmail.com>
2024-09-23 18:19:52 -0300

    Configured react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+
+//Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
