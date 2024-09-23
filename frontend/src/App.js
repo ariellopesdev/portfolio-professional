312bdc13d5824ee29ce09d9c0399f573f938603b
ariellopesdev <lubyel.lopes@gmail.com>
2024-09-23 20:13:27 -0300

    Finished navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,18 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 //Pages
 import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return <div>
+    <ul id="navbar">
+        <li className="active">INÍCIO</li>
+        <li>SOBRE MIM</li>
+        <li>PORTFÓLIO</li>
+        <li>CURRÍCULO</li>
+        <li>CONTATO</li>
+    </ul>
+  </div>;
+};
+
+export default Navbar;
