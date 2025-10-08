7ab9c7d5d3b0336f975cf53c228cb4b90a10d991
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2025-10-07 21:14:56 -0300

    HTML and CSS of the navbar component completed

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -3,18 +3,32 @@ import "./Navbar.css";
 
 //components
 import { NavLink, Link } from "react-router-dom";
-import { FaGithub, FaLinkedin, FaWhatsapp  } from "react-icons/fa";
+
+import { FaGithub, FaLinkedin, FaWhatsapp, FaBars } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <header>
-      <ul>
-        <li><FaGithub /></li>
-        <li><FaLinkedin /></li>
-        <li><FaWhatsapp /></li>
-        <li><MdEmail /></li>
+      <ul id="ul-header">
+        <li>
+          <FaGithub className="nav-icon" />
+        </li>
+        <li>
+          <FaLinkedin className="nav-icon" />
+        </li>
+        <li>
+          <FaWhatsapp className="nav-icon" />
+        </li>
+        <li>
+          <MdEmail className="nav-icon" />
+        </li>
       </ul>
+      <div id="menu-btn">
+        <button id="menu-btn-toggle">
+          <FaBars className="nav-icon"/>
+        </button>
+      </div>
     </header>
   );
 };
