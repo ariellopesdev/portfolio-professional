219775d9f584de06ba7bf91c6882c38eafa3ef27
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2025-10-15 20:14:16 -0300

    changed cards in the projects section v.01

--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -4,6 +4,9 @@ import "./Projects.css";
 //Images
 import testImage from "../../assets/images/image-test.jpg";
 
+//Icons
+import { FaGithub } from "react-icons/fa";
+
 const Projects = () => {
   return (
     <div>
@@ -22,51 +25,66 @@ const Projects = () => {
         </div>
         <div className="block-right">
           <article className="project-card">
-            <img src={testImage} alt="Imagem do projeto RandPlay" />
+            <div className="img-zoom">
+              <img src={testImage} alt="Imagem do projeto RandPlay" />
+            </div>
             <div className="project-card-main">
               <h2>RandPlay</h2>
               <p className="technologys">React.js e Node.js</p>
               <p className="description">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aperiam culpa nesciunt amet tempore consectetur illum asperiores
-                facere! Atque facere enim ipsam, praesentium quisquam impedit
-                neque nam commodi cupiditate fugiat.
+                Aperiam culpa nesciunt amet tempore consectetur illum
+                asperiores.
               </p>
             </div>
-            <div className="btn-link">
-              <button>ver projeto</button>
+            <div className="cta-group">
+              <button className="btn-site">Acessar site</button>
+              <button className="btn-page-project">Acessar projeto</button>
+              <button className="icon-github">
+                <FaGithub />
+              </button>
             </div>
           </article>
           <article className="project-card">
-            <img src={testImage} alt="Imagem do projeto RandPlay" />
+            <div className="img-zoom">
+              <img src={testImage} alt="Imagem do projeto RandPlay" />
+            </div>
             <div className="project-card-main">
               <h2>RandPlay</h2>
               <p className="technologys">React.js e Node.js</p>
               <p className="description">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aperiam culpa nesciunt amet tempore consectetur illum asperiores
-                facere! Atque facere enim ipsam, praesentium quisquam impedit
-                neque nam commodi cupiditate fugiat.
+                Aperiam culpa nesciunt amet tempore consectetur illum
+                asperiores.
               </p>
             </div>
-            <div className="btn-link">
-              <button>ver projeto</button>
+            <div className="cta-group">
+              <button className="btn-site">Acessar site</button>
+              <button className="btn-page-project">Acessar projeto</button>
+              <button className="icon-github">
+                <FaGithub />
+              </button>
             </div>
           </article>
           <article className="project-card">
-            <img src={testImage} alt="Imagem do projeto RandPlay" />
+            <div className="img-zoom">
+              <img src={testImage} alt="Imagem do projeto RandPlay" />
+            </div>
             <div className="project-card-main">
               <h2>RandPlay</h2>
               <p className="technologys">React.js e Node.js</p>
               <p className="description">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Aperiam culpa nesciunt amet tempore consectetur illum asperiores
-                facere! Atque facere enim ipsam, praesentium quisquam impedit
-                neque nam commodi cupiditate fugiat.
+                Aperiam culpa nesciunt amet tempore consectetur illum
+                asperiores.
               </p>
             </div>
-            <div className="btn-link">
-              <button>ver projeto</button>
+            <div className="cta-group">
+              <button className="btn-site">Acessar site</button>
+              <button className="btn-page-project">Acessar projeto</button>
+              <button className="icon-github">
+                <FaGithub />
+              </button>
             </div>
           </article>
         </div>
