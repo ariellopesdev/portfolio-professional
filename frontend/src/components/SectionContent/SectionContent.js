6e123bdb213b3c55292f7937fd7883ecaea03e46
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2025-11-05 19:50:55 -0300

    applied layer effect to section content

--- frontend/src/components/SectionContent/SectionContent.js
+++ frontend/src/components/SectionContent/SectionContent.js
@@ -1,7 +1,19 @@
+//CSS
 import "./SectionContent.css";
 
+//Hooks
+import { useVisibleState } from "../../hooks/useVisibleState";
+
 const SectionContent = () => {
-  return <div className="section-content">SectionContent</div>;
+  const { ref, isVisible } = useVisibleState(0.3);
+
+  return (
+    <div ref={ref} className={`section-content ${isVisible ? "visible" : ""}`}>
+      <div className="layer layer-deep"></div>
+      <div className="layer layer-mid"></div>
+      <div className="layer layer-top"></div>
+    </div>
+  );
 };
 
 export default SectionContent;
