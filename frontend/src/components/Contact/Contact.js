9eb9127cbab5d7bf3212265007f1591f74bc41d3
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2025-11-06 19:37:12 -0300

    Started Contact screen

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,3 +1,4 @@
+//CSS
 import "./Contact.css";
 
 //Components
@@ -5,10 +6,17 @@ import SectionInfo from "../SectionInfo/SectionInfo";
 import SectionContent from "../SectionContent/SectionContent";
 
 //Hooks
+import { useState } from "react";
 import { useVisibleState } from "../../hooks/useVisibleState";
+
+//Icons
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Contact = () => {
   const { ref, isVisible } = useVisibleState(0.3);
+  const [message, setMessage] = useState("");
+  const maxChars = 500;
   return (
     <section id="contact" ref={ref}>
       <div className="sections-wrapper">
@@ -21,19 +29,56 @@ const Contact = () => {
           <form className="contact-form">
             <input type="text" placeholder="Nome" required />
             <input type="email" placeholder="Email" required />
-            <input type="text" placeholder="Assunto" />
-            <textarea placeholder="Mensagem" required></textarea>
-            <button type="submit">Enviar</button>
-
-            <div className="social-icons">
-              <a href="https://linkedin.com/in/seu-link" target="_blank">
-                LinkedIn
-              </a>
-              <a href="https://github.com/seu-usuario" target="_blank">
-                GitHub
-              </a>
-            </div>
+            <textarea
+              placeholder="Mensagem"
+              required
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              maxLength={maxChars}
+            ></textarea>
+            <small>
+              {message.length}/{maxChars} caracteres
+            </small>
+            <button className="btn-form" type="submit">Enviar</button>
           </form>
+          <ul id="ul-contact">
+            <li>
+              <a
+                href="https://github.com/ariellopesdev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub className="nav-icon" />
+              </a>
+              <p>github.com/ariellopesdev</p>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/ariel-lopes-71000821b/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin className="nav-icon" />
+              </a>
+              <p>ariel-lopes-71000821b/</p>
+            </li>
+            <li>
+              <a
+                href="https://wa.me/5532998291774"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp className="nav-icon" />
+                <p>+55 32 99829-1774</p>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:ariel_lopescwb2017@hotmail.com">
+                <MdEmail className="nav-icon" />
+              </a>
+              <p>ariel_lopescwb2017@hotmail.com</p>
+            </li>
+          </ul>
         </SectionContent>
       </div>
     </section>
