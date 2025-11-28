e76c73f2be70475ee72725566bba977b887dc540
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2025-11-27 21:49:05 -0300

    Clean and reviewed code for Aside component
    Standardized comments, brief description of essential places of aside component.

--- frontend/src/components/Aside/Aside.js
+++ frontend/src/components/Aside/Aside.js
@@ -2,7 +2,9 @@
 import "./Aside.css";
 
 const Aside = ({ currentSection }) => {
-  const numSections = 5; // quantidade de traços
+  // Total number of site sections
+  const numSections = 5;
+  // Create an array [0,1,2,3,4] based on the number of sections
   const lines = Array.from({ length: numSections }, (_, i) => i);
 
   return (
@@ -10,6 +12,7 @@ const Aside = ({ currentSection }) => {
       <div id="aside-block">
         <ul>
           {lines.map((line) => (
+            //Each row receives 'active' when its index matches the current section
             <li key={line} className={currentSection === line ? "active" : ""}>
               <span className="line-bar"></span>
             </li>
