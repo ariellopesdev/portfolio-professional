3693a30e45c29e9a26bf5ef3c33cb7f7e1e5d40e
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2025-12-08 21:10:48 -0300

    CSS for pagesgroup completed

--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -10,108 +10,130 @@ import TabsNavigation from "../TabsNavigation/TabsNavigation";
 //Hooks
 import { useVisibleState } from "../../hooks/useVisibleState";
 import useTabsNavigation from "../../hooks/useTabsNavigation";
+import { useNavigate } from "react-router-dom";
 
 //Images
 import ImgTest from "../../assets/images/image-test.jpg";
 
-const blocks = [
-  {
-    content: (
-      <div className="layout-double">
-        <div className="projects-header">
-          <h1 className="projects-title">RandPlay</h1>
-          <h2 className="projects-tech">React.js · Java · MongoDB · APIs</h2>
-        </div>
-
-        <p className="projects-desc">
-          RandPlay é uma plataforma completa para criação e gerenciamento de
-          campeonatos de futebol. Com ela, qualquer pessoa pode montar torneios
-          personalizados, editar regras, registrar resultados, controlar
-          placares e organizar times ou seleções com total liberdade. O usuário
-          ainda pode definir formatos de competição, personalizar escudos e
-          ajustar configurações específicas de cada campeonato.
-        </p>
-
-        <div className="projects-image">
-          <img src={ImgTest} alt="Project image" />
-          <img src={ImgTest} alt="Project image" />
-          <img src={ImgTest} alt="Project image" />
-        </div>
-
-        <div className="projects-links">
-          <a className="btn-projects">Site</a>
-          <a className="btn-projects">Repositório</a>
-        </div>
-      </div>
-    ),
-  },
-  {
-    content: (
-      <div className="layout-double">
-        <div>
-          <h1 className="projects-title">RE Arquitetura</h1>
-          <h2 className="projects-tech">React.js · PHP · Firebase · APIs</h2>
-        </div>
-
-        <p className="projects-desc">
-          Ramon Efísio Arquitetura é um estúdio especializado em projetos
-          arquitetônicos contemporâneos, oferecendo soluções que unem estética,
-          funcionalidade e eficiência espacial. A empresa desenvolve projetos
-          residenciais, comerciais e reformas, sempre priorizando a identidade
-          do cliente e o uso inteligente dos ambientes. Cada trabalho recebe
-          atenção individual, garantindo qualidade técnica, detalhamento preciso
-          e uma experiência arquitetônica personalizada.
-        </p>
-
-        <div className="projects-image">
-          <img src={ImgTest} alt="Project image" />
-          <img src={ImgTest} alt="Project image" />
-          <img src={ImgTest} alt="Project image" />
-        </div>
-
-        <div className="projects-links">
-          <a className="btn-projects">Site</a>
-          <a className="btn-projects">Repositório</a>
-        </div>
-      </div>
-    ),
-  },
-  {
-    content: (
-      <div className="layout-double">
-        <div>
-          <h1 className="projects-title">TecTravel</h1>
-          <h2 className="projects-tech">React.js · Java · MySql · APIs</h2>
-        </div>
-
-        <p className="projects-desc">
-          TechTravel é uma plataforma moderna de viagens que permite planejar e
-          gerenciar roteiros de forma prática e inteligente. Com ela, o usuário
-          pode explorar destinos, montar itinerários personalizados, acompanhar
-          despesas e receber sugestões automatizadas com base em preferências e
-          dados atualizados. Tudo isso em uma experiência visual e tecnológica
-          que torna cada viagem mais simples e personalizada.
-        </p>
-
-        <div className="projects-image">
-          <img src={ImgTest} alt="Project image" />
-          <img src={ImgTest} alt="Project image" />
-          <img src={ImgTest} alt="Project image" />
-        </div>
-
-        <div className="projects-links">
-          <a className="btn-projects">Site</a>
-          <a className="btn-projects">Repositório</a>
-        </div>
-      </div>
-    ),
-  },
-];
+//Icons
+import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
+  const navigate = useNavigate();
   const { ref, isVisible } = useVisibleState(0.3);
-    const { index, anim, goTo} = useTabsNavigation(blocks.length);
-    const tabs = ["RandPlay", "RE Arquitetura", "TechTravel"];
+
+  const blocks = [
+    {
+      content: (
+        <div className="layout-double">
+          <div className="layout-left">
+            <p className="layout-desc-projects">
+              RandPlay é uma plataforma completa para criação e gerenciamento de
+              campeonatos de futebol. Com ela, qualquer pessoa pode montar
+              torneios personalizados, editar regras, registrar resultados,
+              controlar placares e organizar times ou seleções com total
+              liberdade. O usuário ainda pode definir formatos de competição,
+              personalizar escudos e ajustar configurações específicas de cada
+              campeonato.
+            </p>
+            <h3 className="layout-h3">React.js · Java · MongoDB · APIs</h3>
+            <div className="layout-btn-block">
+              <a className="btn-projects" onClick={() => navigate("/projects")}>
+                Ver mais
+              </a>
+              <a
+                className="btn-projects repo-btn"
+                href="https://github.com/seu-repo-aqui"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+          <div className="layout-right">
+            <div className="layout-img-block">
+              <img src={ImgTest} alt="Project image" />
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (
+        <div className="layout-double">
+          <div className="layout-left">
+            <p className="layout-desc-projects">
+              Ramon Efísio Arquitetura é um estúdio especializado em projetos
+              arquitetônicos contemporâneos, oferecendo soluções que unem
+              estética, funcionalidade e eficiência espacial. A empresa
+              desenvolve projetos residenciais, comerciais e reformas, sempre
+              priorizando a identidade do cliente e o uso inteligente dos
+              ambientes.
+            </p>
+            <h3 className="layout-h3">React.js · PHP · Firebase · APIs</h3>
+            <div className="layout-btn-block">
+              <a className="btn-projects" onClick={() => navigate("/projects")}>
+                Ver mais
+              </a>
+              <a
+                className="btn-projects repo-btn"
+                href="https://github.com/seu-repo-aqui"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+          <div className="layout-right">
+            <div className="layout-img-block">
+              <img src={ImgTest} alt="Project image" />
+            </div>
+          </div>
+        </div>
+      ),
+    },
+    {
+      content: (
+        <div className="layout-double">
+          <div className="layout-left">
+            <p className="layout-desc-projects">
+              TechTravel é uma plataforma moderna de viagens que permite
+              planejar e gerenciar roteiros de forma prática e inteligente. Com
+              ela, o usuário pode explorar destinos, montar itinerários
+              personalizados, acompanhar despesas e receber sugestões
+              automatizadas com base em preferências e dados atualizados. Tudo
+              isso em uma experiência visual e tecnológica que torna cada viagem
+              mais simples e personalizada.
+            </p>
+            <h3 className="layout-h3">React.js · Java · MySql · APIs</h3>
+            <div className="layout-btn-block">
+              <a className="btn-projects" onClick={() => navigate("/projects")}>
+                Ver mais
+              </a>
+              <a
+                className="btn-projects repo-btn"
+                href="https://github.com/seu-repo-aqui"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+          <div className="layout-right">
+            <div className="layout-img-block">
+              <img src={ImgTest} alt="Project image" />
+            </div>
+          </div>
+        </div>
+      ),
+    },
+  ];
+
+  const { index, anim, goTo } = useTabsNavigation(blocks.length);
+  const tabs = ["RandPlay", "RE Arquitetura", "TechTravel"];
 
   return (
     <section id="projects" ref={ref}>
