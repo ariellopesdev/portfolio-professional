333e22189abc21f103558bf251cbc06c879303d7
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-02-24 20:07:49 -0300

    Changed font style and some root details for sectionInfo

--- frontend/src/components/SectionInfo/SectionInfo.js
+++ frontend/src/components/SectionInfo/SectionInfo.js
@@ -1,14 +1,12 @@
 import "./SectionInfo.css";
 
-// Two Lines
 import TwoLines from "../TwoLines/TwoLines";
 
 const SectionInfo = ({ title, text, color = "#ff5f5f" }) => {
   return (
-    <div className="section-info">
-      <h1 className="animate-title">{title}</h1>
+    <div className="section__info">
+      <h1 className="info__title">{title}</h1>
       <TwoLines color={color} />
-      {text && <p className="animate-text">{text}</p>}
     </div>
   );
 };
