b057e81e439f428c20beab033e8f7f417c15e4f1
Ariel Lopes <114031261+ariellopesdev@users.noreply.github.com>
2026-03-02 21:46:49 -0300

    Important adjustment for twolines to work

--- frontend/src/components/TwoLines/TwoLines.js
+++ frontend/src/components/TwoLines/TwoLines.js
@@ -1,9 +1,14 @@
+//CSS
 import "./TwoLines.css";
 
-const TwoLines = ({ color = "#ff5f5f", compact = true, variant = "default", }) => {
+import useSectionAnimation from "../../hooks/useSectionAnimation";
+
+const TwoLines = ({ activeSection, index, color = "#ff5f5f", compact = true, variant = "default" }) => {
+  const hasAnimated = useSectionAnimation(activeSection, index);
+
   return (
     <div
-      className={`two-lines ${compact ? "compact" : ""} ${variant}`}
+      className={`two-lines ${compact ? "compact" : ""} ${variant} ${hasAnimated ? "animate" : ""}`}
       style={{ "--line-color": color }}
     />
   );
